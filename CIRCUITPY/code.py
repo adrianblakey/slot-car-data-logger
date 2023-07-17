1d1a013c1373db0119ac7d9d4cc6b3100a9ab903
Adrian Blakey <adrianblakey@gmail.com>
2023-07-17 17:59:13 +0100

    Databases in the browser

--- CIRCUITPY/code.py
+++ CIRCUITPY/code.py
@@ -36,7 +36,8 @@ from adafruit_httpserver import Server, Request, Response, FileResponse, MIMETyp
 
 debug = True
 LANE_COLORS = ['bla', 'pur', 'yel', 'blu', 'ora', 'gre', 'whi', 'red']
-
+ID = "Slot Car Data Logger V1.0"
+GIT = "https://github.com/adrianblakey/slot-car-data-logger"
 
 def scan_wifi_for(ssid: str) -> bool:
     """Scans for a specific ssid"""
@@ -162,12 +163,6 @@ def advertise_service():
             print("MDNS setting failed:", str(e))
 
 
-connect_to_wifi()
-pool = socketpool.SocketPool(wifi.radio)
-server = Server(pool, debug=True, root_path='/static')
-advertise_service()
-
-
 class ConnectedClient:
     def __init__(self, response: SSEResponse = None):
         self._response = response
@@ -216,6 +211,14 @@ class ConnectedClient:
         self._next_message = monotonic() + 1
 
 
+connect_to_wifi()
+pool = socketpool.SocketPool(wifi.radio)
+server = Server(pool, debug=True, root_path='/static')
+advertise_service()
+server.start(str(wifi.radio.ipv4_address))
+
+connected_client = ConnectedClient()
+
 @server.route("/cpu-information", append_slash=True)
 def cpu_information_handler(request: Request):
     """
@@ -231,36 +234,58 @@ def cpu_information_handler(request: Request):
     return JSONResponse(request, cpu_data)
 
 
-connected_client = ConnectedClient()
-os_name = str(time())
 HTML_TEMPLATE = """
 <html lang="en">
     <head>
         <style>
         chart_wrap {
-        position: relative;
-        padding-bottom: 100%;
-        height: 0;
-        overflow:hidden;
+            position: relative;
+            padding-bottom: 100%;
+            height: 0;
+            overflow:hidden;
         }
 
         linechart_material {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width:100%;
-        height:500px;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width:100%;
+            height:600px;
         }
+        
         </style>
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript" src="js/test.js"></script>
         <script type="text/javascript">
             google.charts.load('current', {'packages':['line']});
             google.charts.setOnLoadCallback(init);
-
+            test();
             let db;
+            let INDEXED_DB_SUPPORTED = false;
             let graphDb;
             const DB_NAME = 'logger_db_';
+            let db_name;
             const OS_NAME = 'logger_os';
+
+            const getBrowserName = () => {
+                let browserInfo = navigator.userAgent;
+                let browser;
+                if (browserInfo.includes('Opera') || browserInfo.includes('Opr')) {
+                    browser = 'Opera';
+                } else if (browserInfo.includes('Edg')) {
+                    browser = 'Edge';
+                } else if (browserInfo.includes('Chrome')) {
+                    browser = 'Chrome';
+                } else if (browserInfo.includes('Safari')) {
+                    browser = 'Safari';
+                } else if (browserInfo.includes('Firefox')) {
+                    browser = 'Firefox'
+                } else {
+                    browser = 'unknown'
+                }
+                return browser;
+            }
+
 
             function init() {
 
@@ -271,7 +296,7 @@ HTML_TEMPLATE = """
                 data.addColumn('number', 'Track Voltage');
 
                 var materialOptions = {
-                    title: 'Red Lane Data Logger',
+                    title: 'Data Logger',
                     titlePosition: 'out',
                     titleTextStyle: {fontSize: 28, bold: true},
                     legend: { position: 'out' },
@@ -312,13 +337,51 @@ HTML_TEMPLATE = """
 
                 var chart = new google.charts.Line(document.getElementById('linechart_material'));
 
+                function deleteADatabase(dbName) {
+                    console.log("Deleting database:", dbName);
+                    const DBDeleteRequest = window.indexedDB.deleteDatabase(dbName);
+
+                    DBDeleteRequest.onerror = (event) => {
+                        console.error("Error deleting database.");
+                    };
+
+                    DBDeleteRequest.onsuccess = (event) => {
+                        console.log("Database deleted successfully");
+                    };
+                }
+
                 function initDb() {
                     console.log("initDB");
-                    //const promise = indexedDB.databases();
-                    //    promise.then((databases) => {
-                    //    console.log(databases);
-                    //});
-
+                    const promise = indexedDB.databases();
+                    promise.then((databases) => {
+                        var div = document.getElementById('databases');
+                        var h2 = document.createElement('h2');
+                        h2.textContent = 'Databases in the Browser';
+                        div.appendChild(h2);
+                        var ul = document.createElement('ul');
+                        div.appendChild(ul);
+                        for (var i = 0; i < databases.length; i++) {
+                            var li = document.createElement('li');
+                            var delButton = document.createElement('button');
+                            delButton.innerText = 'delete';
+                            delButton.onclick = function() { // remove list item here
+                                deleteADatabase(this.parentElement.id);
+                                this.parentElement.remove();
+                            };
+                            var disButton = document.createElement('button');
+                            disButton.innerText = 'graph';
+                            disButton.onclick = function() { // graph the database
+                                // TODO graph it
+                            };
+                            li.appendChild(document.createTextNode(databases[i].name));
+                            li.setAttribute("id", databases[i].name);
+                            li.appendChild(delButton);
+                            li.appendChild(disButton);
+                            ul.appendChild(li);
+                            console.log(databases[i].name, databases[i].version);
+                            // deleteADatabase(databases[i].name);
+                        }
+                    });
 
                     let now = new Date();
                     // New db every session
@@ -428,26 +491,30 @@ HTML_TEMPLATE = """
                         firstElapseTime = xVal;
                     }
                     // Handle the mark - only save data between marks?
-                    if (tok[4] == '1') {
-                        if (saveData) {
-                            console.log("mark found stop saving data");
-                            // TODO change the display  to show it's being saved, create an overlay
-                            materialOptions.series[0].lineWidth = 1;
-                            materialOptions.series[1].lineWidth = 1;
-                            materialOptions.series[2].lineWidth = 1;
-                            saveData = false;
-                        } else {
-                            console.log("mark found start saving data");
-                            // TODO change the display  to show it's being saved, create an overlay
-                            materialOptions.series[0].lineWidth = 10;
-                            materialOptions.series[1].lineWidth = 10;
-                            materialOptions.series[2].lineWidth = 10;
-                            saveData = true;
+                    if (INDEXED_DB_SUPPORTED) {
+                        if (tok[4] == '1') {
+                            if (saveData) {
+                                console.log("mark found stop saving data");
+                                materialOptions.series[0].lineWidth = 1;
+                                materialOptions.series[1].lineWidth = 1;
+                                materialOptions.series[2].lineWidth = 1;
+                                hAxis.title = 'Elapse (sec)';
+                                hAxis.textStyle.color = 'black';
+                                saveData = false;    
+                            } else {
+                                console.log("mark found start saving data");
+                                materialOptions.series[0].lineWidth = 10;
+                                materialOptions.series[1].lineWidth = 10;
+                                materialOptions.series[2].lineWidth = 10;
+                                hAxis.title = 'Elapse (sec) - saving data in: ' + db_name;
+                                hAxis.textStyle.color = 'red';
+                                saveData = true;
+                            }
                         }
-                    }
-                    if (saveData) {
-                        console.log('Saving data to database');
-                        addData(tok[0], tok[1], tok[2], tok[3]);
+                        if (saveData) {
+                            console.log('Saving data to database');
+                            addData(tok[0], tok[1], tok[2], tok[3]);
+                        }
                     }
                     console.log('Add row to chart');
                     eTime = (xVal - firstElapseTime) / 1000000000; // scale it to n.xxxxx seconds
@@ -511,7 +578,15 @@ HTML_TEMPLATE = """
                         displayData();
                     });
                 }
-                initDb();
+                const browserName = getBrowserName();
+                if (browserName != 'Firefox') {
+                    console.log("IndexedDB is supported.");
+                    INDEXED_DB_SUPPORTED = true;
+                    initDb();
+                } else {
+                    console.log("IndexedDB is not supported.");
+                }
+                
                 initializeEventSource();
                 chart.draw(data, google.charts.Line.convertOptions(materialOptions));
 
@@ -522,14 +597,48 @@ HTML_TEMPLATE = """
     </head>
     <body>
         <div id="chart_wrap">
-            <div id="linechart_material" style="width: 900px; height: 500px"></div>
+            <div id="linechart_material" style="width: auto; height: 600px"></div>
         </div>
-        <div id="edit_data">
+        <div id="databases_wrap">
+            <div id="databases" style="width: auto">
             <!-- TODO Display the available databases and offer a/c/d/display -->
+            </div>
         </div>
     </body>
 </html>
 """
+
+
+MY_ID = """
+<html lang="en">
+    <head>
+        <title>My ID</title>
+    </head>
+    <body>
+    <h1>""" + ID + """</h1>
+    Hostname: """ + wifi.radio.hostname + """<br>
+    Ip: """ + str(wifi.radio.ipv4_address) + """<br>
+    Git: <a href='""" + GIT + """'>""" + GIT + """</a><br>
+    </body>
+</html>
+"""
+
+MIMETypes.configure(
+    default_to="text/plain",
+    # Unregistering unnecessary MIME types can save memory
+    keep_for=[".html", ".css", ".js", ".png", ".jpg", ".jpeg", ".gif", ".ico"],
+    # If you need to, you can add additional MIME types
+)
+
+
+@server.route("/js/test.js")
+def home(request: Request):
+    return FileResponse(request, "test.js", "/js")
+
+
+@server.route("/id", GET)
+def client(request: Request):
+    return Response(request, MY_ID, content_type="text/html")
 
 
 @server.route("/client", GET)
@@ -547,26 +656,31 @@ def connect_client(request: Request):
     return response
 
 
-server.start(str(wifi.radio.ipv4_address))
+try:
+    pin = digitalio.DigitalInOut(board.GP22)
+    pin.direction = digitalio.Direction.INPUT
+    pin.pull = digitalio.Pull.UP
+    button = Debouncer(pin)
+    while True:
+        pool_result = server.poll()
+        button.update()
+        if button.fell:
+            print('button Just pressed')
+            connected_client.collected = monotonic_ns()
+        elif button.rose:
+            print('button Just released')
+        else:
+            pass
+        if connected_client.ready:
+            connected_client.send_message()
+except Exception as e:
+    print("Error:\n", str(e))
+    print("Resetting mcu in 10 seconds")
+    time.sleep(10)
+    microcontroller.reset()
 
-pin = digitalio.DigitalInOut(board.GP22)
-pin.direction = digitalio.Direction.INPUT
-pin.pull = digitalio.Pull.UP
-button = Debouncer(pin)
 
-while True:
 
-    pool_result = server.poll()
 
-    button.update()
-    if button.fell:
-        print('button Just pressed')
-        connected_client.collected = monotonic_ns()
-    elif button.rose:
-        print('button Just released')
-    else:
-        pass
 
-    if connected_client.ready:
-        connected_client.send_message()
 
